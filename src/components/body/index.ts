f9c508028d68ef27237c2a909e7649e5c7f1f076
Albert Bres <abres@newrelic.com>
2023-07-11 23:16:28 +0200

    added 14 pays

--- src/components/body/index.ts
+++ src/components/body/index.ts
@@ -20,15 +20,15 @@ function updateGrossToNet({ minimal }: { minimal: boolean }) {
     disabilityPercentage,
   } = getFormInputs({ minimal });
 
-  postData(annualGrossSalary, "extranjero", childrenNumber, babiesNumber).then((data) => {
+  postData(annualGrossSalary, "extranjero", childrenNumber, babiesNumber, annualPaymentsNumber).then((data) => {
     console.log(data)
     	
 	  const monthlyNetSalary = Number(data.MonthlyNet)
     const annualNetSalary = Number(data.YearlyNet)
     const annualWithholding = Number(data.TotalWithholdings)
     const annualFee = Number(data.TotalSocialSecurity)
-    const monthlyNetSalaryExtra = 0
-  
+    const monthlyNetSalaryExtra =  Number(data.ExtraMonth)
+
     updateResult({
       monthlyNetSalary,
       annualNetSalary,
@@ -40,10 +40,11 @@ function updateGrossToNet({ minimal }: { minimal: boolean }) {
   
 }
 
-const postData = async (gross_salary: Number, region: string, children: Number, babies: Number) => {
-  var data = { gross_salary: gross_salary, region, children: children, babies: babies}
+const postData = async (gross_salary: Number, region: string, children: Number, babies: Number, annualPaymentsNumber: Number) => {
+  var data = { gross_salary: gross_salary, region, children: children, babies: babies, number_of_payslips: annualPaymentsNumber}
   
-  const response = await fetch('https://api-calcularsueldoneto.fly.dev/netSalary',{
+    const response = await fetch('https://api-calcularsueldoneto.fly.dev/netSalary',{
+    
     method: "POST",
     
     headers: {
